e468f160fa4457e6324adeeedf1fa94ab6628c7c
Oleg Malkov <4.malkov@gmail.com>
2015-12-02 15:24:48 +0300

    lesson 3

--- index.php
+++ index.php
@@ -1,42 +1,44 @@
 
-   <?php      
-     header('Content-type: text/html; charset=utf-8');
-        // Задание 1
-          
-        $name='Олег';   
-        $age=34;
-        echo 'Меня зовут '. $name. "\n";
-        echo 'Мне '. $age. ' года'. "\n";
-        unset ($name, $age);
-        
-            // Задание 2
-
-        define('gorod', 'Покров');
-        if ('gorod'){
-           echo 'Я живу в городе ',gorod, "\n";
-           }else{
-           echo 'Ошибка';
-           }
-        //define('gorod', 'Moskva');
-   
-             // Задание 3
+    <?php
+        header('Content-type: text/html; charset=utf-8');
+        error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
+        ini_set('display_errors', 1);
+    
+        $date = array(
+            rand(1, time()),
+            rand(1, time()),
+            rand(1, time()),
+            rand(1, time()),
+            rand(1, time()),
+        );
+    
+        print_r($date);
+    
+        echo "\n", 'data 1 - '. date ('l d.m.Y H:i:s', $date[0])."\n";
+        echo 'data 2 - '. date ('l d.m.Y H:i:s', $date[1])."\n";
+        echo 'data 3 - '. date ('l d.m.Y H:i:s', $date[2])."\n";
+        echo 'data 4 - '. date ('l d.m.Y H:i:s', $date[3])."\n";
+        echo 'data 5 - '. date ('l d.m.Y H:i:s', $date[4])."\n";
+    
         echo "\n";
-        $book= array('title' => '" Нос "', 'autor' => 'Н.В. Гоголь', 'pages' => '250');
-        echo 'Недавно я прочитал книгу ',$book['title'],' , написанную автором ',$book['autor'],',', "\n";
-        echo 'я осилил все ',$book['pages'],' cтраниц. Мне она очени понравилась.', "\n";
-       
+        echo min(date('d',$date[0]),date('d',$date[1]),date('d',$date[2]),date('d',$date[3]),date('d',$date[4])).' - минимальный день'."\n";
+        echo max(date('m',$date[0]),date('m',$date[1]),date('m',$date[2]),date('m',$date[3]),date('m',$date[4])).' - максимальный месяц'."\n";
+    
+        echo "\n";
+            sort($date);
+            print_r($date);
+    
+        $selected= array_pop($date);
+        print_r('Последняя дата массива - '.$selected);
+    
+        echo "\n", date('l d.m.Y H:i:s', $selected)."\n";
+        echo "\n", date_default_timezone_get();
+        echo "\n", date ('l d.m.Y H:i:s');
+        
+        date_default_timezone_set('America/New_York');
+        echo "\n", date_default_timezone_get();
+        echo "\n", date ('l d.m.Y H:i:s');
+    
 
-            // Задание 4
-        
-        $book1= array('title' => '"Хамелион"', 'autor' => 'А.П. Чехов', 'pages' => '130');
-        $book2= array('title' => '"Евгений Онегин"', 'autor' => 'А.С. Пушкин', 'pages' => '280');
-        $books= array($book1,$book2);
-        
-        //print_r($books);
-                
-        echo "\n", 'Недавно я прочитал две книги: ', $books[0]['title'],' и ',$books[1]['title'],', ', "\n";
-        echo 'написанные соответственно авторами: ',$books[0]['autor'],' и ',$books[1]['autor'],'.', "\n";
-        echo 'я осилил в сумме ',$books[0]['pages'] + $books[0]['pages'],' cтраниц, не ожидал от себя подобного.', "\n";
-        
-               
-       ?>
+    
+?>
